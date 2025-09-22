a6a84c5233b5eebad1f9d778748b9cc78535542b
dwp8546 <mate.fenyvesvoelgy.ext@dwpbank.de>
2025-09-22 14:28:53 +0200

    init first components

---
+++ 01-starting-project/src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+   selector: 'app-header',
+   standalone: true, //in agular 19 or higher versions true value is the default
+   templateUrl: './header.component.html',
+   styleUrls: ['./header.component.css']
+})
+export class HeaderComponent{
+
+}
