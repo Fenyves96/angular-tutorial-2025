c7486b35ed5cd3dbbdd4ac13f9a347227d41ee11
dwp8546 <mate.fenyvesvoelgy.ext@dwpbank.de>
2025-09-23 08:53:50 +0200

    add extra type safe

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -19,8 +19,8 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  //@Output() select = new EventEmitter();
-  select = output<string>(); //this line does tha same thing as the code above it,
+  @Output() select = new EventEmitter<string>();
+  //select = output<string>(); //this line does tha same thing as the code above it,
   // it does not create any signal or something like that!
 
   imagePath = computed(() => {
