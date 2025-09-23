285ceb527af2dfd518f1af6973712869dc22e4bd
dwp8546 <mate.fenyvesvoelgy.ext@dwpbank.de>
2025-09-23 09:07:10 +0200

    show selected user's name

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -2,17 +2,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  onSelectUser(id: string) {
-    console.log(id);
-  }
+  selectedUserId: string = '';
+
   users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
 }
