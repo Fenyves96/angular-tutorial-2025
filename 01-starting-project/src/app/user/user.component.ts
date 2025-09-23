f9d37cc657a1b9dfb31febe3c367a039739d4b58
dwp8546 <mate.fenyvesvoelgy.ext@dwpbank.de>
2025-09-23 08:51:43 +0200

    using output function

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -4,6 +4,7 @@ import {
   EventEmitter,
   Input,
   input,
+  output,
   Output,
 } from '@angular/core';
 
@@ -18,7 +19,9 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  //@Output() select = new EventEmitter();
+  select = output<string>(); //this line does tha same thing as the code above it,
+  // it does not create any signal or something like that!
 
   imagePath = computed(() => {
     return 'assets/users/' + this.avatar;
