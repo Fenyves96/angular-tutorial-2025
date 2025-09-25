3e8d3b7f209b0047bcf269ce9095dec161308648
dwp8546 <mate.fenyvesvoelgy.ext@dwpbank.de>
2025-09-25 21:18:42 +0200

    move CardComponent to shared module

--- 01-starting-project/src/app/app.module.ts
+++ 01-starting-project/src/app/app.module.ts
@@ -1,26 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { CardComponent } from './shared/card/card.component';
-import { DatePipe } from '@angular/common';
-import { TaskComponent } from './tasks/task/task.component';
+import { SharedModule } from './shared/shared.module';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { FormsModule } from '@angular/forms';
+import { TaskComponent } from './tasks/task/task.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    CardComponent,
     TasksComponent,
     TaskComponent,
     NewTaskComponent,
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule],
 })
 export class AppModule {}
