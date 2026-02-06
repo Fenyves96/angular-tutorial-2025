329fdc3fcd1cd50f1907c915d52872fa0aa7e187
dwp8546 <mate.fenyvesvoelgy.ext@dwpbank.de>
2026-02-06 15:38:58 +0100

    162. hostDirective and composition

--- deep-dive-in-directives/src/app/app.component.ts
+++ deep-dive-in-directives/src/app/app.component.ts
@@ -3,14 +3,20 @@ import { Component, computed, inject } from '@angular/core';
 
 import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
-import { AuthDirective } from "./auth/auth.directive";
+import { AuthDirective } from './auth/auth.directive';
+import { LogDirective } from './log.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, AuthDirective, AuthComponent],
+  imports: [
+    AuthComponent,
+    LearningResourcesComponent,
+    AuthDirective,
+    AuthComponent,
+  ],
 })
 export class AppComponent {
   private authService = inject(AuthService);
