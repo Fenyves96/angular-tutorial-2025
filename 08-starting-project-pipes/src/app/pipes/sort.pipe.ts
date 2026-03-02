f29d308682de3612a9ffaccede364373f8f3caef
dwp8546 <mate.fenyvesvoelgy.ext@dwpbank.de>
2026-03-02 17:04:37 +0100

    172-174

--- 08-starting-project-pipes/src/app/pipes/sort.pipe.ts
+++ 08-starting-project-pipes/src/app/pipes/sort.pipe.ts
@@ -3,16 +3,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'sort',
   standalone: true,
+  pure: false, //default is true, it forces angular to not create any cache for the pipe
 })
 export class SortPipe implements PipeTransform {
-  transform(value: string[] | number[] , direction: 'asc' | 'desc' = 'asc'){
+  transform(value: string[] | number[], direction: 'asc' | 'desc' = 'asc') {
     const sorted = [...value];
-    sorted.sort((a,b) => {
-      if(direction === 'asc'){
-        return a>b ? 1:-1;
-      }
-      else{
-        return a>b? -1:1;
+    sorted.sort((a, b) => {
+      if (direction === 'asc') {
+        return a > b ? 1 : -1;
+      } else {
+        return a > b ? -1 : 1;
       }
     });
     return sorted;
