0826a47aabc0a81375fb8672d9481d522d707b6c
dwp8546 <mate.fenyvesvoelgy.ext@dwpbank.de>
2026-03-17 16:10:32 +0100

    204 change detection manually with RxJS subjects

--- 10-change-detection/src/app/messages/messages-list/messages-list.component.ts
+++ 10-change-detection/src/app/messages/messages-list/messages-list.component.ts
@@ -1,13 +1,37 @@
-import { Component, input } from '@angular/core';
+import { MessagesService } from './../message.service';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent {
-  messages = input.required<string[]>();
+export class MessagesListComponent implements OnInit {
+  messages: string[] = [];
+  private destoryRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = this.messagesService.messages$.subscribe(
+      (messages) => {
+        this.messages = messages;
+        this.cdRef.markForCheck();
+      },
+    );
+    this.destoryRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+  private messagesService = inject(MessagesService);
+  private cdRef = inject(ChangeDetectorRef);
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
