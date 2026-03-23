e46f0668881ef5e1e242004c559d82be7b1906a2
dwp8546 <mate.fenyvesvoelgy.ext@dwpbank.de>
2026-03-23 15:14:46 +0100

    add zoneless version of the app

---
+++ change-detection/example-for-view-checking-zoneless/src/app/simple-component/simple-component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-component',
+  imports: [],
+  //All bindings interested in data changing
+  template: `
+    <h2>{{ topicName }}</h2>
+    @if (isVisible) {
+      <div>{{ getInfo() }}</div>
+    }
+  `,
+  styleUrl: './simple-component.scss',
+})
+export class SimpleComponent {
+  topicName = 'Decoded frontend';
+  isVisible = true;
+
+  getInfo() {
+    return 'random info';
+  }
+
+  constructor(private cdr: ChangeDetectorRef) {
+    setTimeout(() => {
+      this.topicName = 'new topic name after timeout';
+      console.log('Topic name changed to', this.topicName);
+      cdr.detectChanges(); // we have to run this manually if we do not have zone.js
+    }, 3000);
+  }
+}
