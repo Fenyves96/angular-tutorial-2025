ef3eb309dfb9421335f87d33b8f792cae2d216a1
dwp8546 <mate.fenyvesvoelgy.ext@dwpbank.de>
2026-03-25 13:06:15 +0100

    signal example and observable

--- change-detection/example-for-view-checking-zoneless/src/app/simple-component/simple-component.ts
+++ change-detection/example-for-view-checking-zoneless/src/app/simple-component/simple-component.ts
@@ -5,19 +5,23 @@ import {
   AfterViewInit,
   inject,
   DestroyRef,
+  signal,
 } from '@angular/core';
 import { ChildComponent } from '../child-component/child-component';
-import { interval, map } from 'rxjs';
+import { BehaviorSubject, interval, map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-simple-component',
-  imports: [ChildComponent],
+  imports: [ChildComponent, AsyncPipe],
   //All bindings interested in data changing
   template: `
     <h2>{{ topicName }}</h2>
     @if (isVisible) {
       <div>{{ getInfo() }}</div>
     }
+    <p>observable variable value: {{ observableVariable$ | async }}</p>
+    <p>signal variable value: {{ signalVariable() }}</p>
     <app-child-component />
   `,
   styleUrl: './simple-component.scss',
@@ -26,16 +30,27 @@ export class SimpleComponent implements AfterViewInit {
   topicName = 'Decoded frontend';
   isVisible = true;
   destroyRef = inject(DestroyRef);
+  cdRef = inject(ChangeDetectorRef);
+  observableVariable$ = new BehaviorSubject<number>(0);
+  signalVariable = signal<number>(0);
 
   ngAfterViewInit(): void {
     const subscription = interval(1000)
-      .pipe(map((val) => val * 2))
+      .pipe(map((val) => val + 1))
       .subscribe({
-        next: (val) => console.log(val),
+        next: (val) => {
+          this.observableVariable$.next(val);
+          this.signalVariable.set(val);
+          this.cdRef.detectChanges;
+        },
       });
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
+
+    setInterval(() => {
+      this.signalVariable.update((val) => val + 1);
+    }, 1 * 1000);
   }
 
   getInfo() {
@@ -43,11 +58,11 @@ export class SimpleComponent implements AfterViewInit {
     return 'random info';
   }
 
-  constructor(private cdr: ChangeDetectorRef) {
+  constructor() {
     setTimeout(() => {
       this.topicName = 'new topic name after timeout';
       console.log('Topic name changed to', this.topicName);
-      cdr.detectChanges(); // we have to run this manually if we do not have zone.js
+      this.cdRef.detectChanges(); // we have to run this manually if we do not have zone.js
     }, 3000);
   }
 }
